8684c179da9179ab633bebe89e34cff211b83eee
Oleksandr Khomiak <khomiak@gmail.com>
2023-05-19 17:17:48 +0300

    edit refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 // імпорт реакту і компоненту реакту
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // імпорт стилізованих компонентів для App
 import { Container } from './App.styled';
@@ -9,106 +9,80 @@ import ContactList from './ContactList';
 import Filter from './Filter';
 import ContactForm from './ContactForm';
 
-// створення нашого компоненту-класу App
-export class App extends Component {
+//------------------
+
+// функціональний компонент на хуках
+export function App() {
+  //
   //наші стейти
-  state = {
-    contacts: [],
-    filter: '',
-    name: '',
-    number: '',
-  };
+  //contacts витягаємо з localStorage і парсимо
+  // якщо нічого нема і приходить null то присвоюємо []
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) ?? []
+  );
+  const [filter, setFilter] = useState('');
 
-  // коли компонент замоунтився - створився
-  // підтягуємо стейт з localStorage,парсимо, але перевіряємо, якщо приходить нулл
-  // то в стейт контактс помістимо пустий масив
-  componentDidMount() {
-    let contactsFromStorage = localStorage.getItem('contacts');
-    let parsedContacts = JSON.parse(contactsFromStorage);
-    // console.log('parsedContacts', parsedContacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
+  // перезаписуємо localStorage коли стейт contacts оновився
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  // коли компонент оновився, перевіряємо умову оновлення стейт
-  // і після цього записуємо нове значення в localStorage
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  // метод, який видаляє наш контакт із стейту
-  // в властивості contacts, що є масивом обʼєктів
-  // отримавши айді елемента
-  deleteContact = index => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(element => element.id !== index),
-      };
-    });
-  };
-
-  // метод, який додає новий контакт в стейт
+  // функція, яка додає новий контакт в contacts
   //в властивість contacts, що є масивом обʼєктів
   // отримавши обʼєкт нового контакту
-  // робить зпрть гп функцію зодо повтору імені
+  // робить запит на функцію щодо повтору імені
   // якщо є збіг - показуєтсья повідомлення
   // якщо збігу немає, то додається новий контакт
-  addContact = newContact => {
-    if (this.checkNewNameRepeate(newContact.name)) {
+  const addContact = newContact => {
+    if (checkNewNameRepeate(newContact.name)) {
       alert(`${newContact.name} is already in contacts!`);
     } else {
-      this.setState(prevState => {
-        return {
-          contacts: [...prevState.contacts, newContact],
-        };
-      });
+      setContacts(prevContacts => [...prevContacts, newContact]);
     }
   };
 
   //перевірка чи є контакт з таким іменем з врахуванням різних регістрів
   // повертає true або false
-  checkNewNameRepeate = newName => {
-    let arrayOfNamesInStateInLowerCase = this.state.contacts.map(item =>
+  const checkNewNameRepeate = newName => {
+    let arrayOfNamesInLowerCase = contacts.map(item =>
       item.name.toLocaleLowerCase()
     );
-    return arrayOfNamesInStateInLowerCase.includes(newName.toLocaleLowerCase());
+    return arrayOfNamesInLowerCase.includes(newName.toLocaleLowerCase());
+  };
+
+  // функцція, яка видаляє наш контакт із contacts,
+  //що є масивом обʼєктів отримавши айді елемента
+  const deleteContact = index => {
+    setContacts(pevContacts =>
+      pevContacts.filter(element => element.id !== index)
+    );
   };
 
   // ф-ія обробник зміни в інфуті фільтра
-  //перезаписує значення filter в state
-  handleChangeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+  //перезаписує значення filter
+  const handleChangeFilter = e => {
+    setFilter(e.currentTarget.value);
   };
 
-  // функція яка готує масив контактів для рендеру
-  //  після відфільтровки за збігом значенням state.filter
-  layOutFilteredContact = () => {
-    return this.state.contacts.filter(item =>
-      item.name
-        .toLocaleLowerCase()
-        .includes(this.state.filter.toLocaleLowerCase().trim())
+  // функція яка готує масив контактів для верстки
+  //  після відфільтровки за збігом значенням filter
+  const layOutFilteredContact = () => {
+    return contacts.filter(item =>
+      item.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase().trim())
     );
   };
 
-  // рендер розмітки
-  render() {
-    return (
-      <Container>
-        <h1>Phonebook</h1>
-        <ContactForm addContact={this.addContact} />
-        <h2>Contacts</h2>
-        <Filter
-          handleChangeFilter={this.handleChangeFilter}
-          value={this.state.filter}
-        />
-        <ContactList
-          contacts={this.layOutFilteredContact()}
-          deleteContact={this.deleteContact}
-        />
-      </Container>
-    );
-  }
+  // розмітка
+  return (
+    <Container>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+      <h2>Contacts</h2>
+      <Filter handleChangeFilter={handleChangeFilter} value={filter} />
+      <ContactList
+        contacts={layOutFilteredContact()}
+        deleteContact={deleteContact}
+      />
+    </Container>
+  );
 }
